b17fa139e23f4d51e93a45cf79752cba6648c12d
Christine Lim <christinelim.clp@gmail.com>
2024-05-08 04:56:16 +0000

    refactored shop products component

--- frontend/src/components/shop-products/ShopProducts.js
+++ frontend/src/components/shop-products/ShopProducts.js
@@ -15,7 +15,7 @@ const ShopProducts = () => {
     const productContext = useContext(ProductContext);
     const sellerContext = useContext(SellerContext);
 
-    const [ products, setProducts ] = useState(null);
+    const [ products, setProducts ] = useState([]);
     const [ showSort, setShowSort ] = useState(false);
     const [ showFilter, setShowFilter ] = useState(false);
     const [ filtered, setFiltered ] = useState(false);
@@ -25,24 +25,20 @@ const ShopProducts = () => {
     const [ currentPage, setCurrentPage ] = useState(1);
 
     useEffect(() => {
-        
         const fetchData = async () => {
-            try {
-                if (isSearchPage) {
-                    let searchTerm;
-                    let searchResult;
+            try { 
+                let searchTerm;
+                let searchResult;
 
-                    if (searchParams.get('product')) {
-                        searchTerm = searchParams.get('product').split('-').join(' ');
-                        searchResult = await productContext.searchProducts({
-                            name: searchTerm
-                        });
-                    } else {
-                        searchTerm = parseInt(searchParams.get('category'));
-                        searchResult = await productContext.searchProducts({
-                            selectedCategories: [searchTerm]
-                        });
-                    }
+                if (isSearchPage) {
+                    searchTerm = searchParams.get('product')
+                       ? searchParams.get('product').split('-').join(' ')
+                        : parseInt(searchParams.get('category'));
+
+                    searchResult = await productContext.searchProducts({
+                       ...(searchTerm? { name: searchTerm } : { selectedCategories: [searchTerm] }),
+                    });
+
                     setSearchedProduct(searchTerm);
 
                     if (searchResult.data.length === 0) {
@@ -52,7 +48,7 @@ const ShopProducts = () => {
                 } else {
                     if (!productContext.loading) {
                         productContext.getProductsByPage(currentPage);
-                        setProducts(productContext.productPage[`page` + currentPage]);
+                        setProducts(productContext.productPage[`page${currentPage}`]);
                     }
                 }
 
@@ -63,48 +59,41 @@ const ShopProducts = () => {
                 }, {});
                 setSellers(sellersMap);
             } catch (error) {
-                console.log(error)
+                console.error(error);
             }
-        }
+        };
 
         fetchData();
-        
     }, [isSearchPage, searchParams, productContext.loading, currentPage, productContext.productPage]);
 
-    const handleSortButton = () => {
-        setShowSort(!showSort)
-    }
-
-    const handleFilterButton = () => {
-        setShowFilter(!showFilter)
-    }
-
+    const handleSortButton = () => setShowSort(!showSort);
+    const handleFilterButton = () => setShowFilter(!showFilter);
     const handleClearFilterButton = () => {
         setFiltered(false);
-        setProducts(productContext.productPage['page1'])
-    }
+        setProducts(productContext.productPage['page1']);
+    };
 
     return (
         <>
             { showFilter && <div className="overlay"></div> }
-            <div className='shop-wrapper'>
-                <div className='page-header'>Shop Products</div>
-                <div className={styles['sort-filter-wrapper']}>
-                    { filtered &&
-                        <div className={styles['sort-filter-button']} onClick={ handleClearFilterButton }>
+            <div className="shop-wrapper">
+                <div className="page-header">Shop Products</div>
+                <div className={styles["sort-filter-wrapper"]}>
+                    { filtered && (
+                        <div className={styles["sort-filter-button"]} onClick={ handleClearFilterButton }>
                             <div>
-                                <i className={`bi bi-x-lg ${styles['sort-filter-icon']}`}></i>
+                                <i className={`bi bi-x-lg ${styles["sort-filter-icon"]}`}></i>
                             </div>
                             <div>Clear Filter</div>
                         </div>
-                    }
-                    <div className={styles['sort-filter-button']} onClick={ handleSortButton }>
+                    )}
+                    <div className={styles["sort-filter-button"]} onClick={ handleSortButton }>
                         <div>
-                            <i className={`bi bi-sort-down ${styles['sort-filter-icon']}`}></i>
+                            <i className={`bi bi-sort-down ${styles["sort-filter-icon"]}`}></i>
                         </div>
                         <div>Sort By</div>
                     </div>
-                    <div className={styles['sort-filter-button']} onClick={ handleFilterButton }>
+                    <div className={styles["sort-filter-button"]} onClick={ handleFilterButton }>
                         <div>
                             <i className="bi bi-funnel sort-filter-icon"></i>
                         </div>
@@ -112,45 +101,45 @@ const ShopProducts = () => {
                     </div>
                 </div>
 
-                { showSort && <SortProducts products={ products } setProducts={ setProducts } /> }
-
-                { showFilter &&
-                    <FilterProducts showFilter={ showFilter} setShowFilter={ setShowFilter } sellers={ sellers }
-                                    setProducts={ setProducts } setEmptySearch={ setEmptySearch }
-                                    searchedProduct={ searchedProduct } setFiltered={ setFiltered }
+                { showSort && <SortProducts products={ products } setProducts={ setProducts } />}
+                { showFilter && (
+                    <FilterProducts
+                        showFilter={ showFilter }
+                        setShowFilter={ setShowFilter }
+                        sellers={ sellers}
+                        setProducts={ setProducts }
+                        setEmptySearch={ setEmptySearch }
+                        searchedProduct={ searchedProduct }
+                        setFiltered={ setFiltered }
                     />
-                }
+                )}
 
-                { emptySearch && <div className={styles['empty-search']}>No products found!</div> }
+                { emptySearch && <div className={styles["empty-search"]}>No products found!</div> }
 
                 { products && (
-                    <div className={styles['products-wrapper']}>
-                        <ProductCards products={ products }/>
+                    <div className={styles["products-wrapper"]}>
+                        <ProductCards products={products} />
                     </div>
                 )}
 
                 { !filtered && productContext.pages && (
-                    <div className={styles['pagination-wrapper']}>
-                        {(() => {
-                            const pagination = [];
-                            for (let page = 1; page <= productContext.pages; page++) {
-                                pagination.push(
-                                    <div
-                                        key={page}
-                                        className={`${styles['page']} ${currentPage === page ? styles['active-page'] : ''}`}
-                                        onClick={() => setCurrentPage(page)}
-                                    >
-                                        {page}
-                                    </div>
-                                );
-                            }
-                            return pagination;
-                        })()}
+                    <div className={styles["pagination-wrapper"]}>
+                        {Array.from({ length: productContext.pages }, (_, i) => (
+                            <div
+                                key={i + 1}
+                                className={`${styles["page"]} ${
+                                    currentPage === i + 1? styles["active-page"] : ""
+                                }`}
+                                onClick={() => setCurrentPage(i + 1)}
+                            >
+                                {i + 1}
+                            </div>
+                        ))}
                     </div>
                 )}
             </div>
         </>
-    )
-}
+    );
+};
 
 export default ShopProducts;
